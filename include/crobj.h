4874bbbae6409ab2d8ea2ac4f82102915efdbf47
Phuong Nguyen <phuongnguyen0793@outlook.com>
2025-12-08 20:20:01 +0700

    feat: add crobj.h
    Signed-off-by: Phuong Nguyen <phuongnguyen0793@outlook.com>

---
+++ include/crobj.h
@@ -0,0 +1,52 @@
+/**
+ * @file crobj.h
+ *
+ */
+
+#ifndef G_CROBJ_H
+#define G_CROBJ_H
+/*********************
+ *      INCLUDES
+ *********************/
+#include <stdint.h>
+
+#include <lvgl.h>
+
+#include "crobj_core.h"
+#include "crobj_grid.h"
+#include "crobj_flex.h"
+#include "crobj_scr.h"
+#include "widget/crobj_widget_menu.h"
+#include "widget/crobj_widget_common.h"
+/*********************
+ *      DEFINES
+ *********************/
+
+/**********************
+ *      TYPEDEFS
+ **********************/
+
+/**********************
+ *  GLOBAL VARIABLES
+ **********************/
+
+/**********************
+ * GLOBAL PROTOTYPES
+ **********************/
+/*=====================
+ * Setter functions
+ *====================*/
+
+/*=====================
+ * Getter functions
+ *====================*/
+
+/*=====================
+ * Other functions
+ *====================*/
+
+/**********************
+ *      MACROS
+ **********************/
+
+#endif /* G_CROBJ_H */
